6002837cece18ec37cdd4a5bb6d057c03c5e99d2
Pradeep Khatri <pradeepkhatri44@live.com>
2025-09-16 16:01:37 +0530

    commit loader and admin login page complete
    commit loader and admin login page complete

--- e-commerce-project-09-2025/src/App.js
+++ e-commerce-project-09-2025/src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { createContext, lazy, Suspense, useEffect, useState } from "react";
+import Loader from "./components/Loader";
+import { ToastContainer } from "react-toastify";
+
+export const AdminLoginContext = createContext();
+
+const AdminLoginCredentials = lazy(() =>
+  import("./modules/admin/AdminLoginCredentials")
+);
+const AdminDashboard = lazy(() => import("./modules/admin/AdminDashboard"));
 
 function App() {
+  const [adminLogin, setAdminLogin] = useState(
+    () => JSON.parse(localStorage.getItem("adminLogin")) || false
+  );
+
+  useEffect(() => {
+    localStorage.setItem("adminLogin", JSON.stringify(adminLogin));
+  }, [adminLogin]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+      <BrowserRouter>
+        <AdminLoginContext.Provider value={{ adminLogin, setAdminLogin }}>
+          <Routes>
+            <Route
+              path="/adminlogin"
+              element={
+                <Suspense fallback={<Loader />}>
+                  <AdminLoginCredentials />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/adminDashboard"
+              element={
+                <Suspense fallback={<Loader />}>
+                  {adminLogin ? (
+                    <AdminDashboard />
+                  ) : (
+                    <Navigate to="/adminLogin" />
+                  )}
+                </Suspense>
+              }
+            />
+          </Routes>
+        </AdminLoginContext.Provider>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ e-commerce-project-09-2025/src/modules/admin/AdminLoginCredentials.js
@@ -0,0 +1,54 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { AdminLoginContext } from '../../App'
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import './AdminLoginCredentials.css';
+
+function AdminLoginCredentials() {
+  const {setAdminLogin} = useContext(AdminLoginContext)
+  const [email, setEmail] = useState("");
+  const [pw, setPw] = useState("");
+  const [data, setData] = useState([]);
+
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    fetch("http://localhost:9999/admin")
+    .then((res)=> res.json())
+    .then((data)=>setData(data))
+    .catch((err)=> console.error("Error fetching Admin data:", err));
+  }, [])
+
+  const handleAdminLogin = (e) => {
+    e.preventDefault();
+    let adminUser = data.find(
+      (v) => v.email === email && v.pw === pw
+    );
+
+    if(adminUser)
+    {
+      toast("Welcome to the Admin Dashboard!");
+      setAdminLogin(true);
+      navigate('/adminDashboard')
+    }
+    else
+    {
+      toast("Invalid Admin Login Credentials, Enter Again")
+    }
+  }
+
+  return (
+    <div className='login-container'>
+      <h2>Admin Login</h2>
+      <form onSubmit={handleAdminLogin}>
+        <label htmlFor="email">Email:</label>
+        <input type="email" value={email} required onChange={(e)=>{setEmail(e.target.value)}} name="" id="emailInp" /><br />
+        <label htmlFor="password">Password:</label>
+        <input type="password" value={pw} required onChange={(e)=>{setPw(e.target.value)}} name="" id="pwInp" /><br />
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default AdminLoginCredentials
